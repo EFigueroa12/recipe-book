4a7b539434730a1535a5f38574b02a98dc44ac0f
EFigueroa12 <efigueroa6912@gmail.com>
2025-02-27 14:38:44 -0800

    can create comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,15 @@
 class CommentsController < ApplicationController
+    def create
+        @comment = current_user.comments.new(comment_params)
+
+        if !@comment.save
+            flash[:notice] = @comment.errors.full_messages.to_sentence
+        end
+        redirect_to recipe_path(params[:recipe_id])
+    end
+
+    private
+    def comment_params
+        params.require(:comment).permit(:context).merge(recipe_id: params[:recipe_id])
+    end
 end
